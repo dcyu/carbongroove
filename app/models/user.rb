02370007aafcd20c5e31bcec0dcf01733ad69a5c
Daniel Yu <dcyu93@gmail.com>
2013-02-13 18:17:08 -0600

    more tests and fixed user#edit route

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
 
   validates :email, :format => /@/
+  validates :password, :length => 5..20
   validates_presence_of :email
   validates_uniqueness_of :email
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,6 +5,16 @@ class UserTest < ActiveSupport::TestCase
     assert_equal [], create_user.receipts
   end
 
+  test "user email is present" do
+    user = create_user
+    assert :email, "User email must be present" 
+  end
+
+  test "user password is present" do
+    user = create_user
+    assert :password, "User email must be present" 
+  end
+
   test "user requires unique email" do
     user = create_user
     assert user.valid?
@@ -13,9 +23,13 @@ class UserTest < ActiveSupport::TestCase
     assert ! user2.valid?
   end
 
-  test "user email must include @ character" do
-    user = User.new email: "asfdkfhjg@asdf.com"
-    assert user.valid?, "must include @"
+
+  test 'user email must include @ and . characters' do
+    user = User.new email: "asfdkfhjg@asdf.com", password: "1q1q1q1q1q1q"
+    assert user.valid?, "must include @ and ."
+  end
+
+  test "password length must be between 5 and 20 characters" do
   end
 
 end
