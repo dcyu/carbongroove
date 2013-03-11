924c5e758f4afb3ea40a7276354679eb2e1bd3bf
QQ5 <slowoasis@gmail.com>
2013-03-11 15:31:34 -0500

    fixed background repeat

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -59,7 +60,6 @@ ActiveRecord::Schema.define(:version => 20130308090346) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "oauth_secret"
-    t.string   "username"
   end
 
 end
