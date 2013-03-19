5742db0971d193456e57db97debe9159bf358199
Daniel Yu <dcyu93@gmail.com>
2013-03-19 13:30:58 -0500

    intuit fixed

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -7,21 +7,6 @@ class Receipt < ActiveRecord::Base
   # validates_inclusion_of :kind, in: KINDS.keys
 
   KINDS = {
-    "electricity" => { cost: 0.155, emission: 2 },
-    "gasoline" => { cost: 4.00, emission: 12.6},
-    "computer" => { cost: 1300.00, emission: 720.00 },
-    "cheeseburger" => { cost: 7.00 , emission: 2.50 },
-    "banana" => { cost: 1.00 , emission: 0.08 },
-    "veggie sandwich" => { cost: 5.00 , emission: 0.40 },
-    "meat sandwich" => { cost: 5.00 , emission: 0.80 },
-    "chicken burrito" => { cost: 7.00 , emission: 0.68 },
-    "beef burrito" => { cost: 7.00 , emission: 3.81 },
-    "french fries" => { cost: 2.00 , emission: 0.81 },
-    "rice" => { cost: 4.00 , emission: 4.00 },
-    "flavored beverage" => { cost: 2.00 , emission: 0.22 },
-    "bottled water" => { cost: 1.00 , emission: 0.16 },
-    "potato chips" => { cost: 1.00 , emission: 0.075 },
-    "corn chips" => { cost: 1.00 , emission: 0.075 },
     #Categories we care about from by Intuit:
     "gas & fuel" => { cost: 4.00 , emission: 12.6 },
     "public transportation" => { cost: 2.50 , emission: 1.5 },
